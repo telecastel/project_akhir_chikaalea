fe2cffc5599728d8533782e1c731756b3e0ecf3b
Chika Alea <chikaalea71@gmail.com>
2025-10-30 14:48:10 +0700

    update server.js

--- backend/server.js
+++ backend/server.js
@@ -51,4 +51,4 @@ app.delete("/api/transaksi/:id", (req, res) => {
     });
 });
 
-app.listen(5000, () => console.log("🚀 Server running on port 5000"));
+app.listen(5000, "0.0.0.0", () => console.log("🚀 Server running on port 5000"));
